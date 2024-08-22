1a833266819bba1bec56fccfa675b28735d70563
9-max-6 <mutukumaxwel@gmail.com>
2024-08-22 12:36:24 +0300

    Chore: algorithm to check if a list of integers represents a UTF-encoded string of characters.

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""module: function validUTF8(data)"""
+
+
+def get_prefix(binary_string):
+    """A function to count the number of 1's
+    at the start of a binary string
+    """
+    count = 0
+    for char in binary_string:
+        if char == '1':
+            count += 1
+    return count
+
+
+def validUTF8(data):
+    """A function to check the validity of data
+    that is supposedly  UTF-8 encoded
+    - Return: True if data is a valid UTF-8 encoding, else return False
+    - A character in UTF-8 can be 1 to 4 bytes long
+    - The data set can contain multiple characters
+    - The data will be represented by a list of integers
+    - Each integer represents 1 byte of data, therefore you only need to
+      handle the 8 least significant bits of each integer
+
+      - format of UTF.
+      1 byte - 0*******
+      2 bytes - [ 11******, 10****** ]
+      3 bytes - 111***** 10****** 10****** 10*****
+      4 bytes - 1111**** 10****** 10****** 10****** 10******
+
+      - strategy
+      the count of ones in the start has to be correspondent to the count
+      of bytes starting with 1 after that.
+      - capture count
+      - loop over the next items in the list and skip items equal to count.
+    """
+    valid = True
+    count = 0
+
+    for index, byte in enumerate(data):
+        if count > 0:
+            count -= 1
+            continue
+
+        if byte <= 127:
+            continue
+
+        binary_rep = bin(byte)[2:]
+
+        count = get_prefix(binary_rep)
+        next_bytes = data[index: index + count]
+        for iter_byte in next_bytes:
+            binary_rep_nested = bin(iter_byte)[2:]
+            binary_check = binary_rep_nested[0:2] == '10'
+            if not binary_check:
+                valid = False
+                return valid
+    return valid
