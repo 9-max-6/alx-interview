2b25b3eee6f694d174af592909cfe720eb6bf8b0
9-max-6 <mutukumaxwel@gmail.com>
2024-09-03 10:30:28 +0300

    Chore: attempted the first question the StarWars API interview question

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,13 +5,42 @@
 // response = ["url1", "url2", ...]
 // url1 = https://swapi-api.alx-tools.com/api/people/1/
 // response =  {"name": "<name>"}
-
+const request = require("/usr/local/lib/node_modules/request");
 const process = require("process");
 
 if (process.argv.length !== 3) {
   console.error("Usage 0-starwars_characters <id>");
 } else {
   id = process.argv[2];
-  url = `https://swapi-api.alx-tools.com/api/${id}/`;
-  console.log(url);
+  url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
+
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      try {
+        const data = JSON.parse(body);
+        data.characters.forEach((url) => {
+          getCharacterName(url);
+        });
+      } catch (e) {
+        console.error("Error parsing JSON:", e);
+      }
+    } else {
+      console.error("Request failed:", error);
+    }
+  });
+}
+
+function getCharacterName(url) {
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      try {
+        const data = JSON.parse(body);
+        console.log(data.name);
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      console.log(response.statusCode);
+    }
+  });
 }
