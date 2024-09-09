8560735539d162c7abed32f9305c246b81de3c7f
9-max-6 <mutukumaxwel@gmail.com>
2024-09-09 10:42:30 +0300

    Fix: to address the issues of results being in the wrong order. I created a series of promises that are awaited asynchronously to save both time and preserver order by awaiting using Promise.all()

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,46 +1,73 @@
 #!/usr/bin/node
-// Parsingc command line arguments
+// Parsing command line arguments
 // Request module though it's depracated
 // url = https://swapi-api.alx-tools.com/api/films/{process.argv[1]}/
 // response = ["url1", "url2", ...]
 // url1 = https://swapi-api.alx-tools.com/api/people/1/
 // response =  {"name": "<name>"}
-const request = require('request');
-const process = require('process');
 
-if (process.argv.length !== 3) {
-  console.error('Usage 0-starwars_characters <id>');
-} else {
-  const id = process.argv[2];
-  const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
+const request = require("/usr/local/lib/node_modules/request");
+const process = require("process");
 
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      try {
-        const data = JSON.parse(body);
-        data.characters.forEach((url) => {
-          getCharacterName(url);
-        });
-      } catch (e) {
-        console.error('Error parsing JSON:', e);
+async function getCharacterUrls(url) {
+  promise = new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else if (response.statusCode === 200 && !error) {
+        try {
+          data = JSON.parse(body);
+          resolve(data.characters);
+        } catch (e) {
+          reject(e);
+        }
+      } else {
+        reject(error);
       }
-    } else {
-      console.error('Request failed:', error);
-    }
+    });
+  });
+
+  try {
+    const characters = await promise;
+    return characters;
+  } catch (e) {
+    new Error("Error fetching URLS");
+  }
+}
+
+async function getCharacters() {
+  // call a function to get the character urls
+  // create promises for each member of the list
+  // after all promises resolve, print the return value.
+  if (process.argv.length !== 3) {
+    console.error("Usage: 0-starwars_characters <filmid>");
+    return;
+  }
+
+  const filmId = process.argv[2];
+  const url = `https://swapi-api.alx-tools.com/api/films/${filmId}/`;
+  let urls = await getCharacterUrls(url);
+  let promises = urls.map((url) => {
+    return new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          try {
+            let character_data = JSON.parse(body);
+            resolve(character_data.name);
+          } catch (e) {
+            reject(e);
+          }
+        } else {
+          reject(error);
+        }
+      });
+    });
+  });
+
+  const character_names = await Promise.all(promises);
+  character_names.forEach((element) => {
+    console.log(element);
   });
 }
 
-function getCharacterName (url) {
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      try {
-        const data = JSON.parse(body);
-        console.log(data.name);
-      } catch (e) {
-        console.log(e);
-      }
-    } else {
-      console.log(response.statusCode);
-    }
-  });
-}
+getCharacters();
