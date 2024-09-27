79dbca21d04e05e2166ddd1428208a60db55dedd
9-max-6 <mutukumaxwel@gmail.com>
2024-09-27 05:14:13 +0300

    Chore: attempted the ALX interview question island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A function to determine the island perimeter
+"""
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of a single island in a grid,
+    where the grid is represented by a 2D array of integers
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
